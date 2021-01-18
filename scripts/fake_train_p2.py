3a2b344a6aee5e71e7c79deb7663cf185363cea0
Thibaut Munzer <thibaut@extrality.ai>
2021-01-18 17:32:03 +0100

    msg

---
+++ scripts/fake_train_p2.py
@@ -0,0 +1,16 @@
+import json
+
+with open("summary.json") as f_in:
+    with open("pipelines/p2/summary.json", "w") as f_out:
+        data = json.load(f_in)
+        print(data)
+        data[
+            "link"
+        ] = "https://github.com/Extrality/extrality-production-ml/pulls?q=is%3Apr+is%3Aclosed"
+        data["toto"] = "test"
+        data["yolo"] = 18
+        json.dump(data, f_out)
+
+with open("plots.csv") as f_in:
+    with open("pipelines/p2/plots.csv", "w") as f_out:
+        f_out.write(f_in.read())
