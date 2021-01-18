860f6b918ffda03f211e18c84c508cefee461f70
Thibaut Munzer <thibaut@extrality.ai>
2021-01-18 16:20:30 +0100

    add link

--- scripts/fake_train.py
+++ scripts/fake_train.py
@@ -1,6 +1,13 @@
+import json
+
 with open("summary.json") as f_in:
     with open("pipelines/p0/summary.json", "w") as f_out:
-        f_out.write(f_in.read())
+        data = json.load(f_in)
+        print(data)
+        data[
+            "link"
+        ] = "https://github.com/Extrality/extrality-production-ml/pulls?q=is%3Apr+is%3Aclosed"
+        json.dump(data, f_out)
 
 with open("plots.csv") as f_in:
     with open("pipelines/p0/plots.csv", "w") as f_out:
